bde43f8e7752771ee4a167f8edba6bb8148edcaa
Francis <francislow_6@hotmail.com>
2020-04-19 05:35:46 +0800

    Add states to dashboard

--- imports/ui/DashBoard.jsx
+++ imports/ui/DashBoard.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import RecordDisplay from './TempPanel'
-import TempPanel from './FloorPanel'
+import React, { useState } from 'react';
+import TempPanel from './TempPanel'
+import FloorPanel from './FloorPanel'
 
 export default function DashBoard() {
+  const [avgTemp, setAvgTemp] = useState(25);
+  const [activeRooms, setActiveRooms] = useState([true, true, true, true, true, true, true]);
   return(
     <div className="content_wrapper">
-      <RecordDisplay />
-      <TempPanel />
+      {/* setActiveRooms passed to TempPanel for testing only */}
+      <TempPanel activeRooms={activeRooms} setAvgTemp={setAvgTemp} setActiveRooms={setActiveRooms}/>
+      <FloorPanel avgTemp={avgTemp} setActiveRooms={setActiveRooms}/>
     </div>
   );
 }

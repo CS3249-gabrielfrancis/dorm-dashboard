91228125e3ee9fdb6af36898aff0352d712a064c
Francis <francislow_6@hotmail.com>
2020-04-18 14:24:46 +0800

    Add dygraph without data inputs

--- imports/ui/RecordDisplay.jsx
+++ imports/ui/RecordDisplay.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Dygraph from 'dygraphs';
 
 export default function RecordDisplay({ numData }) {
+  // Setup dygraph
+  useEffect(() => {
+    new Dygraph('myGraph',
+    `Date,A,B
+    2016/01/01,10,20
+    2016/07/01,20,10
+    2016/12/31,40,30
+    `, {
+      fillGraph: true
+    });
+  }, []);
+  
   return (
     <div className='record_flex'>
-      <h1>RECORD DISPLAY</h1>
-      <h1>Number of data: {numData}</h1>
+      <div className="header_section">
+        <h1>Room Temperatures</h1>
+      </div>
+      <div className="input_section">
+        <h1>INPUT SECTION</h1>
+        <h1>Number of data: {numData}</h1>
+      </div>
+      <div className="graph_section">
+        <h1>GRAPH SECTION</h1>
+        <div id="myGraph"></div>
+      </div>
     </div>
   );
 }

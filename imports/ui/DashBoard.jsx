2942768ebf0a70eaa43cd750572e2c1e428f4d72
Francis <francislow_6@hotmail.com>
2020-04-19 22:25:51 +0800

    Implement userinput for dates and time

--- imports/ui/DashBoard.jsx
+++ imports/ui/DashBoard.jsx
@@ -5,6 +5,7 @@ import FloorPanel from './FloorPanel'
 export default function DashBoard() {
   const [avgTemp, setAvgTemp] = useState(25);
   const [activeRooms, setActiveRooms] = useState([true, true, true, true, true, true, true]);
+
   return(
     <div className="content_wrapper">
       {/* setActiveRooms passed to TempPanel for testing only */}

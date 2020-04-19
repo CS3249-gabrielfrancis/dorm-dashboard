1316d5b65944c0d10d59d520127c70bee0d9478a
Q-gabe <gabriel.ong@u.nus.edu>
2020-04-20 00:12:44 +0800

    add style for appbar and KR crest logo

--- imports/ui/components/MainPanel.jsx
+++ imports/ui/components/MainPanel.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import BodyPanel from './BodyPanel'
-import AppBar from '@material-ui/core/AppBar';
-import Typography from '@material-ui/core/Typography';
-
+import React from "react";
+import BodyPanel from "./BodyPanel";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import { StylesProvider } from "@material-ui/core";
 
 export default function MainPanel() {
-    return (
+  return (
+    <StylesProvider injectFirst>
         <div className="main_panel">
-            <AppBar position="static">
-                <Typography variant="h6" className="Title">
+        <AppBar position="static" className="appbar">
+            <div className="logotitle">
+                <img src="/images/crest.png" alt="logo" className="logo" />
+                <span className="title">
                     Kent Ridge Hall - Temperature Monitoring Dashboard
-                </Typography>
-            </AppBar>
-            <BodyPanel />
+                </span>
+            </div>
+        </AppBar>
+        <BodyPanel />
         </div>
-    )
+    </StylesProvider>
+  );
 }

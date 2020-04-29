9d2f62a0168146b9b45646c48d32d3d51aecbfc9
Q-gabe <gabriel.ong@u.nus.edu>
2020-04-30 07:17:06 +0800

    add styles for TempGraph input section

--- imports/ui/components/MainPanel.jsx
+++ imports/ui/components/MainPanel.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 import BodyPanel from "./BodyPanel";
 import AppBar from "@material-ui/core/AppBar";
-import Typography from "@material-ui/core/Typography";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from '@material-ui/styles';
+
+// Overrides Material UI component font and size for form labels
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: "'Barlow', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+    },
+    overrides: {
+        MuiFormLabel: {
+            root: {
+                fontSize: 'larger',
+            },
+        },
+    },
+});
 
 export default function MainPanel() {
   return (
     //CSS injection order via injectFirst: https://material-ui.com/guides/interoperability/
     <StylesProvider injectFirst>
-        <div className="main_panel">
-        <AppBar position="static" className="appbar">
-            <div className="logotitle">
-                <img src="/images/crest.png" alt="logo" className="logo" />
-                <span className="title">
-                    Kent Ridge Hall - Temperature Monitoring Dashboard
-                </span>
+        <ThemeProvider theme={theme}>
+            <div className="main_panel">
+            <AppBar position="static" className="appbar">
+                <div className="logotitle">
+                    <img src="/images/crest.png" alt="logo" className="logo" />
+                    <span className="title">
+                        Kent Ridge Hall - Temperature Monitoring Dashboard
+                    </span>
+                </div>
+            </AppBar>
+            <BodyPanel />
             </div>
-        </AppBar>
-        <BodyPanel />
-        </div>
+        </ThemeProvider>
     </StylesProvider>
   );
 }
--- imports/ui/containers/TempPanel_InputSection.jsx
+++ imports/ui/containers/TempPanel_InputSection.jsx
@@ -16,57 +16,65 @@ export default function TempPanel_InputSection({
   return (
     <div className='input_section'>
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <Grid container justify='space-around'>
-          <KeyboardDatePicker
-            disableToolbar
-            variant='inline'
-            format='MM/dd/yyyy'
-            margin='normal'
-            id='date-picker-inline'
-            label='Start Date'
-            value={startDate}
-            onChange={(date) => setStartDate(date)}
-            KeyboardButtonProps={{
-              'aria-label': 'change date',
-            }}
-          />
-          <KeyboardDatePicker
-            disableToolbar
-            variant='inline'
-            format='MM/dd/yyyy'
-            margin='normal'
-            id='date-picker-inline'
-            label='End Date'
-            value={endDate}
-            onChange={(date) => setEndDate(date)}
-            KeyboardButtonProps={{
-              'aria-label': 'change date',
-            }}
-          />
-          <KeyboardTimePicker
-            margin='normal'
-            id='time-picker'
-            label='Start Time'
-            value={startDate.getTime()}
-            onChange={(date) => setStartDate(date)}
-            KeyboardButtonProps={{
-              'aria-label': 'change time',
-            }}
-          />
-
-          <KeyboardTimePicker
-            margin='normal'
-            id='time-picker'
-            label='End Timer'
-            value={endDate.getTime()}
-            onChange={(date) => setEndDate(date)}
-            KeyboardButtonProps={{
-              'aria-label': 'change time',
-            }}
-          />
+        <Grid container justify='center' spacing={5} wrap='nowrap'>
+          <Grid item>
+            <Grid container direction="column" justify="center" alignItems="center">
+                <KeyboardDatePicker
+                    variant='inline'
+                    format='MM/dd/yyyy'
+                    margin='normal'
+                    id='date-picker-inline'
+                    label='Start Date'
+                    value={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                    }}
+                />
+                <KeyboardTimePicker
+                    margin='normal'
+                    id='time-picker'
+                    label='Start Time'
+                    value={startDate.getTime()}
+                    onChange={(date) => setStartDate(date)}
+                    KeyboardButtonProps={{
+                    'aria-label': 'change time',
+                    }}
+                />
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container direction="column" justify="center" alignItems="center">
+                <KeyboardDatePicker
+                    disableToolbar
+                    variant='inline'
+                    format='MM/dd/yyyy'
+                    margin='normal'
+                    id='date-picker-inline'
+                    label='End Date'
+                    value={endDate}
+                    onChange={(date) => setEndDate(date)}
+                    KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                    }}
+                />
+                <KeyboardTimePicker
+                    margin='normal'
+                    id='time-picker'
+                    label='End Timer'
+                    value={endDate.getTime()}
+                    onChange={(date) => setEndDate(date)}
+                    KeyboardButtonProps={{
+                    'aria-label': 'change time',
+                    }}
+                />
+            </Grid>
+          </Grid>
+          <Grid item>
+            <SampleSlider sampleSize={sampleSize} setSampleSize={setSampleSize} />
+          </Grid>
         </Grid>
       </MuiPickersUtilsProvider>
-      <SampleSlider sampleSize={sampleSize} setSampleSize={setSampleSize} />
     </div>
   );
 }

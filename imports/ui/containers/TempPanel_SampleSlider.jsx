0a3fbd6d50611db25701e63b0b0a09350347d9f7
Q-gabe <gabriel.ong@u.nus.edu>
2020-04-30 07:16:27 +0800

    add exponential increase function to SampleSlider with matching tooltips

--- imports/ui/containers/TempPanel_SampleSlider.jsx
+++ imports/ui/containers/TempPanel_SampleSlider.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 
 const useStyles = makeStyles({
   root: {
-    width: 400,
+    width: 200,
+    textAlign: 'left',
+    color: 'rgba(0, 0, 0, 0.54)',
   },
 });
+
 
 export default function SampleSlider({sampleSize, setSampleSize}) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Typography id="discrete-slider-small-steps" gutterBottom>
-        Sample Size
-      </Typography>
-      <Slider
-        step={50}
-        marks
-        min={0}
-        max={5000}
-        value={sampleSize}
-        onChange={(event, newValue) => {setSampleSize(newValue);}}
-      
-        valueLabelDisplay="auto"
-      />
+      <div className="sample_slider">
+        <div className="sample_slider_header">
+            <span className="sampleTitle">Samples</span>
+            <span className="sampleCount">{sampleSize}</span>
+        </div>
+        <Slider
+            marks
+            min={1}
+            step={1}
+            max={12}
+            defaultValue={6}
+            onChange={(event, newValue) => {setSampleSize(2 ** newValue);}}  
+            valueLabelFormat={(x) => "2^"+x}
+            valueLabelDisplay="auto"
+            getAriaValueText={(x) => "2^"+x} 
+            aria-labelledby="non-linear-slider"
+        />
+      </div>
     </div>
   );
 }

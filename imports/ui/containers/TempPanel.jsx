56509c460f0a9711606b89cdb7b19c979217b7a3
Q-gabe <gabriel.ong@u.nus.edu>
2020-05-01 02:59:49 +0800

    add zoom feature on dygraph && refactor dygraph interactionModel option

--- imports/ui/containers/TempPanel.jsx
+++ imports/ui/containers/TempPanel.jsx
@@ -60,24 +60,6 @@ export default function TempPanel({activeRooms, setAvgTemp}) {
     }
   }, [activeRooms]);
 
-  return (
-    <div className='panel temp_panel record_flex'>
-      <Header />
-      <InputSection 
-        startDate={startDate}
-        setStartDate={setStartDate}
-        endDate={endDate}
-        setEndDate={setEndDate}
-        sampleSize={sampleSize}
-        setSampleSize={setSampleSize} 
-      />
-      <div className='graph_section'>
-        <div id='TempGraphLegend'></div>
-        <div id='TempGraph'></div>
-      </div>
-    </div>
-  );
-
   // Dirtily assembles the legend from the chart data
   // Note: The 'dirty' html concatenation is due to the string interpretation of the
   // dygraphs legendFormatter option. (https://github.com/danvk/dygraphs/pull/683)
@@ -110,51 +92,109 @@ export default function TempPanel({activeRooms, setAvgTemp}) {
     });
     
     return html+'</div>';
-      // return (
-      //   '<div class="legendTitle">'+date+'</div>' +
-      //   data.series
-      //   .map(v => v.dashHTML + v.labelHTML + ': ' + v.yHTML)
-      //   .join(' ')
-      // );
   }
 
+  // Defines all the user interactions with the graph
+  const graphInteractions = {
+    // Zoom(Shift/alt held)/Pan starts on mouse down
+    mousedown: (event, g, context) => {
+      context.initializeMouseDown(event, g, context);
+      if (event.altKey || event.shiftKey) {
+        Dygraph.startZoom(event, g, context);
+      } else { 
+        Dygraph.startPan(event, g, context);
+      } 
+    },
+    // Pan moves with mouse / Tracks mouse to mark zoom region
+    mousemove: (event, g, context) => {
+      if(context.isPanning) {
+        Dygraph.movePan(event, g, context);
+      } else if (context.isZooming) {
+        Dygraph.moveZoom (event, g, context);
+      }
+    },
+    // Mouse up ends pan movement / releases zoom region to enact zoom
+    mouseup: (event, g, context) => {
+      if (context.isZooming) {
+        Dygraph.endZoom(event, g, context);
+      } else if (context.isPanning) {
+        Dygraph.endPan(event, g, context);
+      }
+      setStartDate(new Date(g.xAxisRange()[0]))
+      setEndDate(new Date(g.xAxisRange()[1]))
+    },
+    // Mouse leaves graph area, if zooming or panning, do the same as a mouseup event at context
+    mouseleave: (event, g, context) => {
+      if (context.isZooming) {
+        Dygraph.endZoom(event, g, context);
+        setStartDate(new Date(g.xAxisRange()[0]))
+        setEndDate(new Date(g.xAxisRange()[1]))
+      } else if (context.isPanning) {
+        Dygraph.endPan(event, g, context);
+        setStartDate(new Date(g.xAxisRange()[0]))
+        setEndDate(new Date(g.xAxisRange()[1]))
+      }
+    },
+    mousewheel: (event, g, context) => {
+      var zoomIn = event.wheelDelta > 0 ? true : false;
 
+      zoom(g, zoomIn);
+      setStartDate(new Date(g.xAxisRange()[0]))
+      setEndDate(new Date(g.xAxisRange()[1]))
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  };
+
+  // Calculates the new x axis bounds upon mousewheel event.
+  // zoomIn is a boolean that represents whether it is a zoom in or out event
+  function zoom(g, zoomIn) {
+    var axis = g.xAxisRange();
+    var range = axis[1] - axis[0];
+
+    // The asymmetric increase/decrease on the right is due to the
+    // a perceived right bias upon scrolling. The right increase by 0.01
+    // provides a counterweight to the zoom bias.
+    var leftIncrement = range * 0.025;
+    var rightIncrement = range * 0.035;
+
+    // Assign increments according to zoom in or zoom out
+    var newAxis = zoomIn? [axis[0] + leftIncrement, axis[1] - rightIncrement] 
+      : [axis[0] - leftIncrement, axis[1] + rightIncrement]
+
+    // update x axis
+    g.updateOptions({
+      dateWindow: newAxis
+    });
+  }
+
+  // Creates the Dygraph
   function plotGraph(initialData) {
     return new Dygraph('TempGraph', initialData, {
-      // legend: 'always',
-      // title: 'Temperature vs. Time',
       labels: ['Date', 'R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6'],
       labelsDiv: 'TempGraphLegend',
       legendFormatter: legendFormatter,
       legend: 'always',
       animatedZooms: true,
-      interactionModel: {
-        mousedown: (event, g, context) => {
-          context.initializeMouseDown(event, g, context);
-          if (event.altKey || event.shiftKey) {
-            Dygraph.startZoom(event, g, context);
-          } else { 
-            Dygraph.startPan(event, g, context);
-          } 
-        },
-        mousemove: (event, g, context) => {
-          if(context.isPanning) {
-            Dygraph.movePan(event, g, context);
-          } else if (context.isZooming) {
-            Dygraph.moveZoom (event, g, context);
-          }
-        },
-        mouseup: (event, g, context) => {
-          if (context.isZooming) {
-            Dygraph.endZoom(event, g, context);
-          } else if (context.isPanning) {
-            Dygraph.endPan(event, g, context);
-          }
-
-          setStartDate(new Date(g.xAxisRange()[0]))
-          setEndDate(new Date(g.xAxisRange()[1]))
-        }
-      }
+      interactionModel: graphInteractions
     });
   }
+
+  return (
+    <div className='panel temp_panel record_flex'>
+      <Header />
+      <InputSection 
+        startDate={startDate}
+        setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+        sampleSize={sampleSize}
+        setSampleSize={setSampleSize} 
+      />
+      <div className='graph_section'>
+        <div id='TempGraphLegend'></div>
+        <div id='TempGraph'></div>
+      </div>
+    </div>
+  );
 }

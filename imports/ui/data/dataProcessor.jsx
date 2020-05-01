3a1f9c118de462a3ffc69c8d0650fb5fdb171aee
Francis <francislow_6@hotmail.com>
2020-05-01 19:52:54 +0800

    Fixed dygraph drawing issue and clean code

--- imports/ui/data/dataProcessor.jsx
+++ imports/ui/data/dataProcessor.jsx
@@ -1,4 +1,41 @@
-export function getReducedData(data, sampleSize) {
+export function processData(data, sampleSize) {
+  const reducedData = reduceData(data, sampleSize);
+  const sortedData = sortData(reducedData);
+  return reformatData(sortedData);
+}
+
+export function findAverageTemp(data) {
+  let totalRoomTemp = [0, 0, 0, 0, 0, 0, 0];
+  let totalRoomCount = [0, 0, 0, 0, 0, 0, 0];
+  for (let i = 0; i < data.length; i++) {
+    let currentRoomTemp = [
+      data[i].roomTemps.room0, 
+      data[i].roomTemps.room1, 
+      data[i].roomTemps.room2, 
+      data[i].roomTemps.room3, 
+      data[i].roomTemps.room4, 
+      data[i].roomTemps.room5, 
+      data[i].roomTemps.room6
+    ];
+    for (let j = 0; j <= 6; j++) {
+      if (currentRoomTemp[j] != null) {
+        totalRoomTemp[j] += currentRoomTemp[j];
+        totalRoomCount[j] += 1;
+      }
+    }
+  }
+  return [
+    totalRoomTemp[0] / totalRoomCount[0],
+    totalRoomTemp[1]  / totalRoomCount[1],
+    totalRoomTemp[2]  / totalRoomCount[2],
+    totalRoomTemp[3]  / totalRoomCount[3],
+    totalRoomTemp[4]  / totalRoomCount[4],
+    totalRoomTemp[5]  / totalRoomCount[5],
+    totalRoomTemp[6]  / totalRoomCount[6],
+  ];
+}
+
+function reduceData(data, sampleSize) {
   let filteredData = [];
   // Filter data based on sample size
   if (sampleSize != 0) {
@@ -19,7 +56,11 @@ export function getReducedData(data, sampleSize) {
   return filteredData;
 }
 
-export function getReformatedData(data) {
+function sortData(data) {
+  return data.slice().sort((a, b) => a.date - b.date)
+}
+
+function reformatData(data) {
   const dataToBeFed = [];
   for (let i = 0; i < data.length; i++) {
     dataToBeFed.push([
@@ -34,67 +75,4 @@ export function getReformatedData(data) {
     ]);
   }
   return dataToBeFed;
-}
-
-export function getAverageTemp(data) {
-  let totalRoom0Temp = 0;
-  let totalRoom1Temp = 0;
-  let totalRoom2Temp = 0;
-  let totalRoom3Temp = 0;
-  let totalRoom4Temp = 0;
-  let totalRoom5Temp = 0;
-  let totalRoom6Temp = 0;
-  let totalRoom0Count = 0;
-  let totalRoom1Count = 0;
-  let totalRoom2Count = 0;
-  let totalRoom3Count = 0;
-  let totalRoom4Count = 0;
-  let totalRoom5Count = 0;
-  let totalRoom6Count = 0;
-  for (let i = 0; i < data.length; i++) {
-    let currentRoom0Temp = data[i].roomTemps.room0;
-    let currentRoom1Temp = data[i].roomTemps.room1;
-    let currentRoom2Temp = data[i].roomTemps.room2;
-    let currentRoom3Temp = data[i].roomTemps.room3;
-    let currentRoom4Temp = data[i].roomTemps.room4;
-    let currentRoom5Temp = data[i].roomTemps.room5;
-    let currentRoom6Temp = data[i].roomTemps.room6;
-    if (currentRoom0Temp != null) {
-      totalRoom0Temp += currentRoom0Temp;
-      totalRoom0Count += 1;
-    }
-    if (currentRoom1Temp != null) {
-      totalRoom1Temp += currentRoom1Temp;
-      totalRoom1Count += 1;
-    }
-    if (currentRoom2Temp != null) {
-      totalRoom2Temp += currentRoom2Temp;
-      totalRoom2Count += 1;
-    }
-    if (currentRoom3Temp != null) {
-      totalRoom3Temp += currentRoom3Temp;
-      totalRoom3Count += 1;
-    }
-    if (currentRoom4Temp != null) {
-      totalRoom4Temp += currentRoom4Temp;
-      totalRoom4Count += 1;
-    }
-    if (currentRoom5Temp != null) {
-      totalRoom5Temp += currentRoom5Temp;
-      totalRoom5Count += 1;
-    }
-    if (currentRoom6Temp != null) {
-      totalRoom6Temp += currentRoom6Temp;
-      totalRoom6Count += 1;
-    }
-  }
-  return [
-    totalRoom0Temp / totalRoom0Count,
-    totalRoom1Temp / totalRoom1Count,
-    totalRoom2Temp / totalRoom2Count,
-    totalRoom3Temp / totalRoom3Count,
-    totalRoom4Temp / totalRoom4Count,
-    totalRoom5Temp / totalRoom5Count,
-    totalRoom6Temp / totalRoom6Count,
-  ];
 }
